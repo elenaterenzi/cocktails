0736fd88cee062ef0218b7ef9ad7f699db2819aa
Elena Terenzi <you@example.com>
2023-07-22 11:39:05 +0200

    added ingreditents to cocktail list

--- Models/CocktailView.cs
+++ Models/CocktailView.cs
@@ -2,6 +2,7 @@ namespace cocktails.Models{
     public class CocktailView{
         public int Id {get;set;}
         public string Nome {get;set;}
+        public string Ingredienti {get;set;}
         public bool Alcool {get;set;}
     }
 }
